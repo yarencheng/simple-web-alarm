6396a9ffc0b69851a66eeafdc484564841da4684
Aren Chen <yarencheng@gmail.com>
2020-08-07 12:34:25 +0800

    using ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Typography, Row, Col } from 'antd';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IProps {
 }
 
-export default App;
+interface IState {
+
+}
+
+export default class App extends React.Component<IProps, IState> {
+
+  render() {
+    return (
+      <Row
+        justify='center'
+      >
+        <Col
+          span={24}
+        >
+          <Typography.Title
+            style={{
+              textAlign: 'center',
+            }}
+          >
+            Simple Web Alarm
+          </Typography.Title>
+        </Col>
+      </Row>
+
+    );
+  }
+}
+
