3850b6a9f32a41610b02f9e4e8048b9e47c1d860
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-07 09:25:12 +0000

    Deploy yarencheng/simple-web-alarm to github.com/yarencheng/simple-web-alarm.git:gh-pages

---
+++ precache-manifest.589fd61b0ae59301cdd52881daf133bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c139c7f0a40dbf2cdd9bdba80302602",
+    "url": "/simple-web-alarm/index.html"
+  },
+  {
+    "revision": "f0e5ad6802052506745e",
+    "url": "/simple-web-alarm/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "30b19de6c5dce9e642cd",
+    "url": "/simple-web-alarm/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "f0e5ad6802052506745e",
+    "url": "/simple-web-alarm/static/js/2.d21abcbc.chunk.js"
+  },
+  {
+    "revision": "b2d18f4208d077ff9abeffd009ec7cac",
+    "url": "/simple-web-alarm/static/js/2.d21abcbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b19de6c5dce9e642cd",
+    "url": "/simple-web-alarm/static/js/main.77e43416.chunk.js"
+  },
+  {
+    "revision": "ccc9741d96e791a2d74b",
+    "url": "/simple-web-alarm/static/js/runtime-main.50b542d7.js"
+  }
+]);
